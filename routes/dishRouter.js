6202a53fb027d6371c0a84ff7b9f75aa82c50be2
patel.p <patel.p@PANKIT-LAPTOP>
2021-05-12 16:21:21 +1200

    ASsignment 1

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const dishRouter = express.Router();
 dishRouter.use(bodyParser.json());
 
+//calls for /dishes
 dishRouter
   .route("/")
   .get((req, res, next) => {
@@ -23,6 +24,29 @@ dishRouter
   })
   .delete((req, res, next) => {
     res.end("Deleting all the dishes!");
+  });
+
+//calls for dishes/:dishId
+dishRouter
+  .route("/:dishId")
+  .get((req, res, next) => {
+    res.end("Will send details of the dish: " + req.params.dishId + " to you!");
+  })
+  .post((req, res, next) => {
+    res.statusCode = 403;
+    res.end("POST operation not supported on /dishes/" + req.params.dishId);
+  })
+  .put((req, res, next) => {
+    res.write("Updating the dish: " + req.params.dishId + "\n");
+    res.end(
+      "Will update the dish: " +
+        req.body.name +
+        " with details: " +
+        req.body.description
+    );
+  })
+  .delete((req, res, next) => {
+    res.end("Deleting dish: " + req.params.dishId);
   });
 
 module.exports = dishRouter;
